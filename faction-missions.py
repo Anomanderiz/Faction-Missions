3e7b28321425075867666176d8ba408c2df45dc7
Anomanderiz <91763965+Anomanderiz@users.noreply.github.com>
2025-09-20 01:31:20 -0700

    Update faction-missions.py

--- faction-missions.py
+++ faction-missions.py
@@ -494,7 +494,7 @@ import traceback, json
 def main():
     st.set_page_config(page_title=APP_TITLE, page_icon="🗺️", layout="wide", initial_sidebar_state="collapsed")
     set_glass_background(BACKGROUND_IMAGE, overlay_strength=0.38, vignette=0.30, blur_px=0)
-    tweak_top_bar("glass") # try 'glass' or 'hidden' if you prefer
+    tweak_top_bar("hidden") # try 'glass' or 'hidden' or 'compact' if you prefer
     chrome_header()
 
     if "selected_mission_id" not in st.session_state:
